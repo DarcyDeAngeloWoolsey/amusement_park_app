e2fd62058eb367274186b158030ce81213c7db70
Darcy DeAngelo-Woolsey <Bucketlist2>
2017-08-22 22:34:59 -0400

    modified getRecentRideUpdates test12

--- public/app.js
+++ public/app.js
@@ -116,7 +116,7 @@ $(document).ready(function () {
 
     //add ability to get list from database instead
     $(function () {
-        /*getRecentRideUpdates();*/
+        getRecentRideUpdates(data.RideStatus);
         $(".formList").submit(function () {
             event.preventDefault();
             getRecentRideUpdates();
