28938b0cb0e4eb77529a2aee50fced17a46bdb87
Darcy DeAngelo-Woolsey <Bucketlist2>
2017-08-28 15:52:13 -0400

    testing get query15

--- public/app.js
+++ public/app.js
@@ -148,7 +148,7 @@ $(document).ready(function () {
         $(".formList").submit(function () {
             event.preventDefault();
             getRecentRideUpdates();
-
+            $(".list").show();
         });
     });
 
