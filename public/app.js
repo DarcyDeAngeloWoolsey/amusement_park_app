e12581300f4aed6eb7d0a0144b046181a54851b8
Darcy DeAngelo-Woolsey <Bucketlist2>
2017-08-29 12:08:38 -0400

    testing app,js and server.js delete method3

--- public/app.js
+++ public/app.js
@@ -101,6 +101,14 @@ function displayRideUpdates(data) {
 
     $(".btnDelete").click(function (event) {
         event.preventDefault();
+        $(this).siblings(function {
+            $.get('/rides', {
+                amusementParkName: $('[name=list]').val()
+            }, function (data) {
+                console.log("running get rides in delete button");
+            });
+        });
+
         $(".modalDelete").show();
     });
 
@@ -201,13 +209,16 @@ $(document).ready(function () {
     $(".buttonDeleteYes").click(function () {
         event.preventDefault();
         console.log("running buttonDeleteYes in app");
-        $.delete('/rides/:id', function (data) {
+
+        //need to figure out how to get the id of this data that we want to delete.
+
+        /*$.delete('/rides/:id', function (data) {
             console.log("postRides working");
             displayRideUpdates(data);
             $(".modalDelete").hide();
         });
         (".list").empty();
-        $(".list").show();
+        $(".list").show();*/
     });
 
     $(".buttonDeleteNo").click(function () {
